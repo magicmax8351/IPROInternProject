4813f2f49fdfb010fb4310853dd06df2fb8634e1
Justin Schmitz <jschmitz2@hawk.iit.edu>
2020-09-22 19:22:55 -0500

    Initalize React, FastAPI

---
+++ ipro-intern-project/backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+import os
+import image_gen
+import sheet_data
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
+            
