c4913f45e156f11d0c9de5899dfc51cb4d85c990
hunter-negron <hnegron@hawk.iit.edu>
2020-10-09 12:35:18 -0500

    added user and post tables CRUD functions

--- ipro-intern-project/backend/main.py
+++ ipro-intern-project/backend/main.py
@@ -26,11 +26,57 @@ class Item(BaseModel):
     price: float
     is_offer: Optional[bool] = None
 
+# CRUD functions for each table
+# User
+@app.post("/users/add")
+def add_user(new_user: UserModel):
+    """Adds a new row to user table."""
+    raise HTTPException(400, "Not implemented")
+
+@app.get("/users/get")
+def get_user(user_id: int):
+    """Returns a user object with the given ID."""
+    raise HTTPException(400, "Not implemented")
+
+@app.post("/users/update")
+def update_user(updated_user: UserModel):
+    """Updates the user with the given ID with new information.
+       Checks to make sure that the user exists first."""
+    raise HTTPException(400, "Not implemented")
+
+@app.post("/users/delete")
+def delete_user(user_id: int):
+    """Removes the user with the given ID."""
+    raise HTTPException(400, "Not implemented")
+
+# Post
+@app.post("/posts/add")
+def add_post(new_post: PostModel):
+    """Adds a new row to post table."""
+    raise HTTPException(400, "Not implemented")
+
+@app.get("/posts/get")
+def get_post(post_id: int):
+    """Returns a post object with the given ID."""
+    raise HTTPException(400, "Not implemented")
+
+@app.post("/posts/update")
+def update_post(updated_post: PostModel):
+    """Updates the post with the given ID with new information.
+       Checks to make sure that the post exists first."""
+    raise HTTPException(400, "Not implemented")
+
+@app.post("/posts/delete")
+def delete_post(post_id: int):
+    """Removes the post with the given ID."""
+    raise HTTPException(400, "Not implemented")
+
+# END HUNTER'S CODE
+
 @app.get("/groups/get")
 def get_group(group_id: int):
     """Returns a group object with the given ID. """
     raise HTTPException(400, "Not implemented")
-
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
