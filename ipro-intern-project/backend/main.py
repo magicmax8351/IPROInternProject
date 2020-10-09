2c7712293c533e523e7319ae420ce9adaa29ff18
hunter-negron <hnegron@hawk.iit.edu>
2020-10-09 13:30:52 -0500

    finished CRUD skeleton functions for each table

--- ipro-intern-project/backend/main.py
+++ ipro-intern-project/backend/main.py
@@ -356,9 +356,3 @@ def update_presetitem(updated_presetitem: PresetitemModel):
 def delete_presetitem(presetitem_id: int):
     """Removes the presetitem with the given ID."""
     raise HTTPException(400, "Not implemented")
-
-# END HUNTER'S CODE
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
