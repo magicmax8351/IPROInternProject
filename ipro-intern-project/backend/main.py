27c225c6a645c9c30587cb4fed73fb8893c05563
Justin Schmitz <jschmitz2@hawk.iit.edu>
2021-03-11 11:55:12 -0600

    hotfix

--- ipro-intern-project/backend/main.py
+++ ipro-intern-project/backend/main.py
@@ -524,6 +524,7 @@ def get_jobs(token: str):
             j[job.id] = job
             job.key = job.id
             job.tags = []
+            job.company = get_company_id(job.company_id)
         
         if(p[1]):
             j[job.id].tags.append(tag_map[p[1].tag_id])
